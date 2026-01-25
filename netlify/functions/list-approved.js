c6edbf1321c2d78f2389329520853a390e943f2b
panel-parade <demonbeavis@gmail.com>
2026-01-25 12:56:28 +0000

    Update list-approved.js

--- netlify/functions/list-approved.js
+++ netlify/functions/list-approved.js
@@ -1,13 +1,10 @@
 exports.handler = async () => {
   try {
-    // TODO: connect to R2 and list files in the "approved/" folder
-    // TODO: load metadata/gallery.json and merge with filenames
-
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "list-approved.js is wired up and ready",
-        approved: [] // will be filled with real data later
+        message: "list-approved.js is wired up",
+        approved: []
       })
     };
   } catch (err) {
